a877f2d88d725453d9f202361c9ed6e538b276f8
ANAGHA <anaghashibu14@gmail.com>
2025-08-31 12:43:03 +0530

    Add files via upload

---
+++ questline-3/longest_palindrom.py
@@ -0,0 +1,15 @@
+y=input()
+x=y.split()
+l=[]
+for i in x:
+    if i==i[::-1]:
+        l.append(i)
+largest=len(l[0])
+for i in range(0,len(l)):
+               j=len(l[i])
+               if j>largest:
+                   k=i
+                   largest=j
+print(l[k])
+
+
