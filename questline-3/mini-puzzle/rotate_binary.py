6c96c689ef22771b021a173a513fdf76bdf99c77
ANAGHA <anaghashibu14@gmail.com>
2025-09-02 21:29:20 +0530

    Add files via upload

--- questline-3/mini-puzzle/rotate_binary.py
+++ questline-3/mini-puzzle/rotate_binary.py
@@ -1,1 +1,23 @@
+s=input()
+k=int(input())
+y=len(s)
+direction=input("Enter left or right")
+if direction.upper()=="RIGHT":
+    for i in range(k):
+        s=s[y-1]+s[0:y-1]
+        sum=0
+    for i in range(y):
+        a=int(s[i])*2**(y-1-i)
+        sum=sum+a
+    print(sum)
+else:
+    for i in range(k):
+        s=s[1:y]+s[0]
+        sum=0
+    for i in range(y):
+        a=int(s[i])*2**(y-1-i)
+        sum=sum+a
+    print(sum)
+    
 
+    
